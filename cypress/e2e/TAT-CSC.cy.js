bf56a29489e730f5e4050e90b5bb83eb2dae21f2
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-10-18 14:16:33 +0200

    adding other flavors on example 1

--- cypress/e2e/TAT-CSC.cy.js
+++ cypress/e2e/TAT-CSC.cy.js
@@ -1,9 +1,13 @@
 describe('TAT Customer Service Center', () => {
-  it('checks the application title - equal', () => {
+  it('checks the application title - eq', () => {
     cy.visit('./src/index.html');
     cy.title().should('eq', 'TAT Customer Service Center');
   });
 
+  it('checks the application title - be.equal', () => {
+    cy.visit('./src/index.html');
+    cy.title().should('be.equal', 'TAT Customer Service Center');
+  });
   it('checks the application title - include', () => {
     cy.visit('./src/index.html');
     cy.title().should('include', 'TAT Customer');
