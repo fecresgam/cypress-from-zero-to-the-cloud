d7dbc281b07a41742d3e20d1bcb8fdd1a83964e0
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-11-01 09:06:58 +0100

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   //video: true, //Saves video
+  projectId: "d19kq9",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
